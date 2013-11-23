0cb431391634aa8ec6d7edf8bc50abea67a1dc3c
Amy Stephen <AmyStephen@gmail.com>
2013-11-22 21:13:10 -0600

    Initial Molajo Interfaces

---
+++ RouteInterface.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Route Interface
+ *
+ * @package    Route
+ * @copyright  2013 Common Api. All rights reserved.
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+namespace Api\Route;
+
+/**
+ * Route Interface
+ *
+ * @package    Route
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @copyright  2013 Common Api. All rights reserved.
+ * @since      1.0
+ */
+interface RouteInterface
+{
+    /**
+     * Determine if secure protocol required and in use
+     *
+     * @return  object
+     * @since   1.0
+     */
+    public function verifySecureProtocol();
+
+    /**
+     * Determine if request is for home page
+     *
+     * @return  object
+     * @since   1.0
+     */
+    public function verifyHome();
+
+    /**
+     * Set Action from HTTP Method
+     *
+     * @return  object
+     * @since   1.0
+     */
+    public function setRequest();
+
+    /**
+     * Set Route
+     *
+     * @return  object
+     * @since   1.0
+     */
+    public function setRoute();
+}
